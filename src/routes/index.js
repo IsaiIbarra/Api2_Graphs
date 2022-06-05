75753516c17a68c15a711823b527fc7870f69f41
IsaiIbarra <isaiibarra1010@gmail.com>
2022-06-04 23:38:32 -0500

    Add API of get distance to each restaurant

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,4 +24,34 @@ router.get('/', (req, res) => {
   });
 });
 
+//API get distance to each restaurant
+router.post('/getRestaurantDistances', (req, res) => {
+  const { id_res_a } = req.body;
+  console.log(req.body);
+  mysqlConnection.query(
+    'SELECT distances.*, restaurants_a.name_res as name_res_a, restaurants_b.name_res as name_res_b, units.unit_uni FROM distances INNER JOIN restaurants as restaurants_a ON distances.id_res_a = restaurants_a.id_res INNER JOIN restaurants as restaurants_b ON distances.id_res_b = restaurants_b.id_res INNER JOIN units ON units.id_uni = distances.id_uni WHERE id_res_a = ?',
+    [id_res_a],
+    (err, rows, fields) => {
+      if (!err) {
+        for (let i = 0; i < rows.length; i++) {
+          if (rows[i].distance_dis == null) {
+            rows[i].distance_dis = 'Unknown';
+            console.log(rows[i].distance_dis);
+          } else {
+            rows[i].distance_dis =
+              rows[i].distance_dis + ' ' + rows[i].unit_uni;
+          }
+        }
+        res.json({
+          result: true,
+          message: 'Successful Query!',
+          distances: rows,
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
 module.exports = router;
