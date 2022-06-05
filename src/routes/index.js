3c095b0fff585aa5851f89534113a5ef63ef5252
IsaiIbarra <isaiibarra1010@gmail.com>
2022-06-05 01:49:15 -0500

    Remove unnecessary comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,6 @@ router.use(cors());
 
 //API get all restaurants
 router.get('/', (req, res) => {
-  //   res.json({ Titulo: 'Adios' });
   mysqlConnection.query('SELECT * FROM restaurants', (err, rows, fields) => {
     if (!err) {
       res.json({
@@ -27,7 +26,6 @@ router.get('/', (req, res) => {
 //API get distance to each restaurant
 router.post('/getRestaurantDistances', (req, res) => {
   const { id_res_a } = req.body;
-  console.log(req.body);
   mysqlConnection.query(
     'SELECT distances.*, restaurants_a.name_res as name_res_a, restaurants_b.name_res as name_res_b, units.unit_uni FROM distances INNER JOIN restaurants as restaurants_a ON distances.id_res_a = restaurants_a.id_res INNER JOIN restaurants as restaurants_b ON distances.id_res_b = restaurants_b.id_res INNER JOIN units ON units.id_uni = distances.id_uni WHERE id_res_a = ?',
     [id_res_a],
@@ -36,7 +34,6 @@ router.post('/getRestaurantDistances', (req, res) => {
         for (let i = 0; i < rows.length; i++) {
           if (rows[i].distance_dis == null || rows[i].distance_dis == 0) {
             rows[i].distance_dis = 'Unknown';
-            console.log(rows[i].distance_dis);
           }
         }
         res.json({
@@ -54,7 +51,6 @@ router.post('/getRestaurantDistances', (req, res) => {
 //API for insert distance between restaurants
 router.post('/insertDistance', (req, res) => {
   const { id_res_a, id_res_b, distance_dis } = req.body;
-  console.log(req.body);
   mysqlConnection.query(
     'UPDATE distances SET distance_dis = ? WHERE id_res_a = ? and id_res_b = ?',
     [distance_dis, id_res_a, id_res_b],
